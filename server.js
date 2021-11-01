5e6ce83e4d2cbc85900721051a2b0040bdd7b6d3
akscarface20091 <39053232+akscarface20091@users.noreply.github.com>
2021-10-31 23:46:14 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+const cors = require('cors');
+const dbConnection = require('./database/connection');
+
+dotEnv.config();
+
+const app = express();
+
+dbConnection();
+
+app.use(cors());
+
+// request payload middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res, next) => {
+    res.send('hello from node server')
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`); 
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {}
+    });
+});
