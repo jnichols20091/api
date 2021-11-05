2ecbb9891f55713494d5c26add592e317a4ccabc
akscarface20091 <39053232+akscarface20091@users.noreply.github.com>
2021-11-05 00:44:17 -0500

    4th

--- controller/productController.js
+++ controller/productController.js
@@ -1,3 +1,18 @@
-module.exports.createProduct = (req, res) => {
-    res.send('product created successfully!')
+const productService = require('../service/productService');
+
+
+module.exports.createProduct = async (req, res) => {
+    let response = {};
+    try {
+        const responseFromService = await productService.createProduct(req.body);
+        response.status = 200;
+        response.message = 'product created sucessfully';
+        response.body = responseFromService;
+    } catch (error) {
+        console.log('something went wrong: Controller: createProduct', error);
+        response.status = 400;
+        response.message = error.message;
+        response.body = {};
+    }
+    return res.status(response.status).send(response);
 }
---
+++ service/productService.js
@@ -0,0 +1,13 @@
+const Product = require('../database/models/productModel');
+
+module.exports.createProduct = async (serviceData) => {
+    try {
+        let product = new Product({...serviceData });
+        return await product.save();
+    } catch (error){    
+        console.log('something went wrong: Service: createProduct', error);
+        throw new Error(error);
+}
+}
+
+
